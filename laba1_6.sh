03c89866ce9f6c49515c561eefd315e470cf4fae
Denis345 <51526615+Denis345@users.noreply.github.com>
2022-02-17 22:52:58 +0300

    Update laba1_6.sh

--- laba1_6.sh
+++ laba1_6.sh
@@ -1,8 +1,8 @@
 if [ $# -eq 4 ] ; then 
 	find $1 -type f -size +$2b -size -$3b -printf "%p, %f, %s\n" > $4
-	printf "Number of viewed files:" ; find $1 -type f | wc -l
+	printf "Number of files viewed " ; find $1 -type f | wc -l
 else
-	echo "Wrong number of parameters!" > &2
-	echo "Command format: (serch directory) (min size) (max size) (out file name)" > &2
+	echo "number of invalid parameters" > &2
+	echo "format: (serch directory) (min_size) (max_size) (out_file name)" > &2
 fi
 
