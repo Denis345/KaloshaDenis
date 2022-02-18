0da91010abf4538a46ef2e315335c99880040bd7
Denis345 <51526615+Denis345@users.noreply.github.com>
2022-02-18 20:05:12 +0300

    Update task7.sh

--- task7.sh
+++ task7.sh
@@ -3,11 +3,5 @@
 original=$1;
 compiled=$2;
 
-gcc $original -o $compiled.exe
 
-if [[ $? -eq 0 ]]
-then
-	./$compiled.exe
-else
-	echo "Check the type of your file"	
-fi
+gcc $original -o $compiled.exe && ./$compiled.exe
